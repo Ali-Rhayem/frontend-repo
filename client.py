05b41c398585a475a070cdf233fcb532255462d2
Alirh1 <alirhayem.1212@gmail.com>
2024-07-07 12:47:27 +0300

    added error message in case server is not running on local host

--- client.py
+++ client.py
@@ -8,7 +8,8 @@ class Client:
     def run(self):
         if self.config['ServerIPAddress'] == '127.0.0.1':
             print("Running on localhost")
-
+        else:
+            print("error: Not running on localhost")
 if __name__ == "__main__":
     config_path = os.path.join(os.path.dirname(__file__), '../config.yaml')
     with open(config_path, 'r') as file:
